cabe37df5108784743c34a5d86913f63492bd815
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-20 11:19:45 -0500

    added some components... jumbotron content appearing on the page

---
+++ directory/src/components/Jumbotron/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div>
+            <h1>Employee Directory</h1>
+            <p>
+                This is a modified jumbotron that occupies the entire horizontal space of
+                its parent.
+            </p>
+        </div>
+    )
+};
+
+
+
+
+
+export default Jumbotron;
